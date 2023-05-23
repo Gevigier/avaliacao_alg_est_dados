747dacd2bd51afe8a98a27b8430e21e99f5bd090
Marcos G <117698420+Gevigier@users.noreply.github.com>
2023-05-23 11:54:08 -0300

    final_cadastro
    Finalizei a tela de cadastro. Caso não haja um arquivo CSV com os usuários, se cria um e, se já houver, apenas adiciona a linha. Criei uma função para ser usada como tela de confirmação por todo o código, modifiquei algumas orientações de desenvolvimento.
    
    Há também um sistema para verificar se foi realmente inserido um e-mail correto.

--- avaliacaoAlg.py
+++ avaliacaoAlg.py
@@ -1,11 +1,15 @@
 import pandas as pd
+import os
+import re
 
 class sistemaLoja:
 
     def __init__(self):
-        print('inicializado')
+        # Inicialização
         sistemaLoja.telaInicial(self)
         
+
+
     def telaInicial(self):
         # Tela inicial
         print('''
@@ -13,78 +17,87 @@ class sistemaLoja:
                   Seja bem-vindo a nossa loja!
 
         Escolha uma opção:
-        1. Entrar                       2. Cadastre-se
-                                        0. Encerrar''')
+        [1] Entrar                       [2] Cadastre-se
+                                         [0] Encerrar''')
         
         while True:
             try:
                 resp = int(input())
                 match resp:
                     case 0:
-                        print('fechar')
+                        saida = True
                     case 1:
                         print('entrar')
                     case 2:
-                        print('cadastrar')
                         sistemaLoja.telaCadastro(self)
                 
                 break
             except:
                 print('Opção incorreta. Insira um número referente a uma das opções disponíveis.')
 
+        if saida == True:
+            exit()
+
+
+
+    def telaConfirmacao(self):
+        # Tela de confirmação para ser usada e reciclada por todo o código
+        while True:
+            opcoes = int(input('''
+            [1] Confirmar                    [2] Cancelar
+                        '''))
+
+            match opcoes:
+                case 1:
+                    return 1
+                case 2:
+                    return 0
+                case _:
+                    print('Opção incorreta. Insira um número referente a uma das opções disponíveis. (func separada)') 
+
 
     def telaCadastro(self):
 
         # Tela de Cadastro
         print('''
-        -------------- CADASTRO DE USUÁRIO ------------''')
-
-        while True:
-            try:
-                # TENTA ABRIR A BASE DE DADOS (ARQUIVO CSV) DE USUÁRIOS
-                user_db = pd.read_csv('./user_db.csv', sep=';')
-            except:
-                # CASO NÃO CONSIGA, CRIA UMA NOVA BASE DE DADOS VAZIA
-                user_db = pd.DataFrame({'Nome':[],
-                'E-mail':[],
-                'Senha': [],
-                'Nascimento': [],
-                'CPF': []})
-                break
+        -------------- CADASTRO DE USUÁRIO ------------
+        ''')
 
         nomeUser    = str(input('NOME E SOBRENOME: '))
-        emailUser   = str(input('INSIRA UM E-MAIL: ')) # Criar um sistema de teste pra ver se é um e-mail?
+
+        while True:
+            emailUser   = str(input('INSIRA UM E-MAIL: '))
+            
+            if (not re.fullmatch(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", emailUser)) == False:
+                break
+            else:
+                print('''
+        A informação inserida não se refere a um email. Por favor, tente novamente.
+        ''')
+                
         senhaUser   = str(input('CRIE UMA SENHA: ')) 
         nascUser    = input('DATA DE NASCIMENTO: ') # Alguma forma de armazenar data?
         cpfUser     = str(input('CPF: ')) # Alguma forma de verificar se é um cpf?
 
-        # append data frame to CSV file
-        # user_db.to_csv('./user_db.csv', mode='a', index=False, header=False, sep=';')
-                       
-
-
-
-
-        while True:
-            try:
-
-                print('1. Confirmar     2. Cancelar')
-                confirmacao = int(input())
-
-                match confirmacao:
-                    case 1:
-                        print('confirmar')
-
-                    case 2:
-                        print('cancelar')
-                        sistemaLoja.telaInicial(self)
-
-                break
-            except:
-                print('Opção incorreta. Insira um número referente a uma das opções disponíveis.')        
+        new_user = pd.DataFrame({'Nome':[nomeUser],
+        'E-mail':[emailUser],
+        'Senha': [senhaUser],
+        'Nascimento': [nascUser],
+        'CPF': [cpfUser]})
         
-
-
+        if self.telaConfirmacao() == 1:
+            if (os.path.isfile('./user_db.csv')) is True:
+                new_user.to_csv('./user_db.csv', mode='a', index=False, header=False, sep=';')
+            else:
+                new_user.to_csv('./user_db.csv', mode='a', index=False, header=True, sep=';')
+        else:
+            print('''
+                   -x- Operação Cancelada -x-
+                  Retornando a tela inicial...
+                ''')
+            sistemaLoja.telaInicial(self)
+            
+    
 
     
     def resto():
@@ -92,14 +105,13 @@ class sistemaLoja:
         print('''
         -------------- ENTRADA DE USUÁRIO -------------''')
 
-        email      = str(input('INSIRA UM E-MAIL: ')) # Criar um sistema de teste pra ver se é um e-mail?
-        senha      = str(input('CRIE UMA SENHA: ')) # Teste para quão forte é a senha?
+        email      = str(input('INSIRA SEU E-MAIL: ')) # Criar um sistema de teste pra ver se é um e-mail?
+        senha      = str(input('INSIRA SUA SENHA: ')) # Teste para quão forte é a senha?
 
-        confirmacao = str(input('1. Confirmar     2. Cancelar')) # Fazer esquema de insistir até que seja a opção correta
 
 
 
 
 if __name__ == "__main__":
-
+    # Este IF serve para que o código seja executado
     sistema = sistemaLoja()
